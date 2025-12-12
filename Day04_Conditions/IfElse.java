88f32323beaf7aa05c58ff1af80bb634fb0b44db
lezyCoder <iamvijaysingh23@gmail.com>
2025-12-13 04:47:20 +0530

    Day04_Completed

---
+++ Day04_Conditions/IfElse.java
@@ -0,0 +1,14 @@
+public class IfElse {
+
+   public static void main(String args[]) {
+      int x = 30;
+
+      if( x < 20 ) {
+         System.out.print("This is if statement");
+      }else {
+         System.out.print("This is else statement");
+      }
+   }
+}
+
+// There are many others types of If else block too read it ...
